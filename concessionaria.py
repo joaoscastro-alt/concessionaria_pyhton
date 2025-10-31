90f610a74f0c671dd1a6929b66b65b1e8aab50be
JOAO PEDRO SANTANA DE CASTRO <joao.scastro@aluno.senac.rio>
2025-10-31 18:24:47 -0300

    concessionaria

---
+++ concessionaria.py
@@ -0,0 +1,6 @@
+idade_visitante = int(input("insira a idade\n"))
+
+if idade_visitante >= 18:
+    print("Seja bem vindo! Vou apresentar nosso melhores veículos.")
+else:
+    print("Seja bem vindo! Vou levá-lo para o espaço kids.")
